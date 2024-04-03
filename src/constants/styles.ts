4adadd10744254632434c6abfaebdac3eb16c375
Yelynn Oh <yoyelynn@gmail.com>
2024-04-03 17:48:27 +0900

    #17 chore: style const 추가

---
+++ src/constants/styles.ts
@@ -0,0 +1,41 @@
+interface stylesProps {
+  id: string;
+  faceShape: string;
+  hairStyle: string[];
+  description: string;
+}
+
+export const STYLES: stylesProps[] = [
+  {
+    id: 'Heart',
+    faceShape: '역삼각형',
+    hairStyle: ['단발 C컬펌', '아이롱펌', '보브컷'],
+    description:
+      '앞머리 있는 단발 혹은 \n 볼륨이 많은 헤어가 이 유형에 속해요.',
+  },
+  {
+    id: 'Long',
+    faceShape: '긴형',
+    hairStyle: ['레이어드컷', '히피펌', '중단발 레이어드펌'],
+    description: '사이드에 볼륨이 많은 \n 중단발의 헤어가 이 유형에 속해요.',
+  },
+  {
+    id: 'Round',
+    faceShape: '둥근형',
+    hairStyle: ['스트레이트', '숏컷', '태슬컷'],
+    description: '직선 느낌을 낼 수 있는 \n 스트레이트 헤어가 이 유형에 속해요',
+  },
+  {
+    id: 'Square',
+    faceShape: '사각형',
+    hairStyle: ['단발 레이어드펌', '허쉬컷', '샤밍컷'],
+    description:
+      '앞머리가 없고 5:5 가르마의, \n 층이 있는 단발의 헤어가 이 유형에 속해요.',
+  },
+  {
+    id: 'Oval',
+    faceShape: '계란형',
+    hairStyle: ['C컬펌 장발', '빌드펌', '사이드뱅'],
+    description: '내추럴한 느낌의 시스루뱅이나 \n 펌이 이 유형에 속해요.',
+  },
+];
